6695f8a374c0ae08ad1b57a8d65b2e3e8d717a8b
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-12-21 21:45:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+const para = document.getElementById("count");
+function inc() {
+    let count = Number(para.innerText); 
+	alert(count); 
+	para.innerText = ++count; 
+}
